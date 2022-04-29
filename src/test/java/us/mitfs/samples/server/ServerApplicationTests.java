b7701664fcd24854cae969796a0ceccbf83e748d
MIT-FS <irm@us.es>
2022-04-29 08:57:52 +0200

    Esqueleto del proyecto

---
+++ src/test/java/us/mitfs/samples/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package us.mitfs.samples.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
