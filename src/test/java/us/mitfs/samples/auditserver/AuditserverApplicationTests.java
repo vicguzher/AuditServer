2b105a06d253451b11178f47f0ad24b09636d363
Sergio Millan Rodriguez <sergio.millan@impala.travel>
2022-05-02 13:44:29 +0200

    feat: add /healthz endpoint tests

--- src/test/java/us/mitfs/samples/auditserver/AuditserverApplicationTests.java
+++ src/test/java/us/mitfs/samples/auditserver/AuditserverApplicationTests.java
@@ -1,13 +1,75 @@
 package us.mitfs.samples.auditserver;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Map;
 
 @SpringBootTest
 class AuditserverApplicationTests {
 
+	@Autowired
+	private AppController controller;
+
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
 	}
 
 }
+
+@SpringBootTest
+class SmokeTest {
+
+	@Autowired
+	private AppController controller;
+
+	@Test
+	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+}
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+class SuccessfulHealthzTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void healthz() throws Exception {
+		String endpoint = "http://localhost:" + port + "/healthz";
+		Map<String, Object> endpointResponse = this.restTemplate.getForObject(endpoint, Map.class);
+		assertThat(endpointResponse).containsKeys("healthy", "check");
+		assertThat(endpointResponse.get("healthy")).isEqualTo(true);
+	}
+}
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, properties = { "app.githubToken=faketoken" })
+class UnsuccessfulHealthzTest {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void getUnsuccessfulHealthz() throws Exception {
+		String endpoint = "http://localhost:" + port + "/healthz";
+		Map<String, Object> endpointResponse = this.restTemplate.getForObject(endpoint, Map.class);
+		assertThat(endpointResponse).containsKeys("healthy", "error");
+		assertThat(endpointResponse.get("healthy")).isEqualTo(false);
+	}
+}
+
