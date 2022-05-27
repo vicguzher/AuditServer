87dddb527fc6d26eb07a372f867ccd02a9aa9fee
MIT-FS <irm@us.es>
2022-05-27 17:10:57 +0200

    arreglando el pipeline

--- scripts/localenv/win-deploy.ps1
+++ scripts/localenv/win-deploy.ps1
@@ -9,13 +9,13 @@ $version_line = (Select-String -Path "build.gradle" -Pattern 'version =').ToStri
 $version = $version_line.split(' ')[-1].replace("'", "")
 Write-Output $version
 Write-Output "Lee variable de entorno GITHUB_OAUTH"
-$github_login = [System.Environment]::GetEnvironmentVariable('GITHUB_LOGIN')
 $github_oauth = [System.Environment]::GetEnvironmentVariable('GITHUB_OAUTH')
+$github_login = [System.Environment]::GetEnvironmentVariable('GITHUB_LOGIN')
 Write-Output "Ejecuta kind load docker-image para desplegar la aplicación en el cluster kubernetes"
 kind load docker-image audit-server:$version --name audit-server
 #kind load docker-image audit-server --name audit-server
 Write-Output "Ejecuta helm upgrade para configurar el cluster"
-helm upgrade --install audit-server ./deploy/charts/audit-server -f ./deploy/local.yaml --set image.tag="$version" --set config.githubOauth="$github_oauth",config.githubLogin="$github_login" 
+helm upgrade --install audit-server ./deploy/charts/audit-server -f ./deploy/local.yaml --set image.tag="$version" --set config.githubOauth="$github_oauth" --atomic --timeout 3m0s
 Write-Output "Ejecuta kubectl port para conectar el puerto 80 del cluster al 8080 de la máquina anfitriona"
 kubectl port-forward svc/audit-server 8080:80
-exit $lastexitcode
+exit $lastexitco
